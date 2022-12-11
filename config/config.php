6382471e32a3ea31a92af1b9258a849c9c51db4d
Gabriel Musbah <87495580+GabrielMusbah@users.noreply.github.com>
2022-12-11 09:52:58 -0400

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$smarty = new Smarty();
+
+$smarty->setTemplateDir('./templates/');
+$smarty->setCompileDir('./templates_c/');
+$smarty->setCacheDir('./cache/');
+$smarty->setConfigDir('./config/');
+
+//$smarty->testInstall();
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+/*
+Criar as pastas:
+    - templates
+    - templates_c (compilado)
+    - cache
+    - config
+*/
+
+require 'config/config.php';
+
